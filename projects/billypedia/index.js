bbaf57faea1c56e67fbee877697db116b66ba19b
Jared Geraci <zeta9520@gmail.com>
2016-11-17 00:26:47 +0000

    added videos and purple theme

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -2,26 +2,26 @@
 $(document).ready(function() {
     $.getJSON('data.json', function (data) {
         // YOUR CODE BELOW HERE //
-        $('#section-discography').css('background-color', '#635ACC').css;
+        $('#section-discography').css('background-color', '#BC36FF').css;
         $('#section-discography').css('color', 'white');
         $('#section-discography').css('padding-right', '10px').css('padding-left', '10px');
         $('#section-discography').css('padding-bottom', '2px');
         
-        $('#section-quotes').css('background-color', '#635ACC').css;
-        $('#section-quotes').css('color', 'white');
-        $('#section-quotes').css('padding-right', '10px').css('padding-left', '10px');
-        $('#section-quotes').css('padding-bottom', '2px');
-        $('.quote').css('color', 'white').css('font-style', 'italic');
+        //$('#section-quotes').css('background-color', '#635ACC').css;
+        //$('#section-quotes').css('color', 'white');
+        //$('#section-quotes').css('padding-right', '10px').css('padding-left', '10px');
+        //$('#section-quotes').css('padding-bottom', '2px');
+        //$('.quote').css('color', 'white').css('font-style', 'italic');
         
-        $('#section-praise').css('background-color', '#635ACC').css;
-        $('#section-praise').css('padding-right', '10px').css('padding-left', '10px');
-        $('#section-praise').css('padding-bottom', '2px');
-        $('#section-praise').css('color', 'white');
+        //$('#section-praise').css('background-color', '#635ACC').css;
+        //$('#section-praise').css('padding-right', '10px').css('padding-left', '10px');
+        //$('#section-praise').css('padding-bottom', '2px');
+        //$('#section-praise').css('color', 'white');
         
-        $('#section-bio').css('background-color', '#635ACC').css;
-        $('#section-bio').css('padding-right', '10px').css('padding-left', '10px');
-        $('#section-bio').css('padding-bottom', '2px');
-        $('#section-bio').css('color', 'white')
+        //$('#section-bio').css('background-color', '#635ACC').css;
+        //$('#section-bio').css('padding-right', '10px').css('padding-left', '10px');
+        //$('#section-bio').css('padding-bottom', '2px');
+        //$('#section-bio').css('color', 'white')
         
         $('#section-bio p:last-child').remove();
 
